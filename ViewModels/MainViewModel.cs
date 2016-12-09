d09d10cb3bde106ee9c0915a3f8e83b7eef9f7a1
D4rthB4n3 <dessel.bane@gmx.de>
2016-12-09 12:27:36 +0100

    Started work on Basic View Models

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModels
+{
+    public class MainViewModel
+    {
+        private readonly ObservableCollection<FileViewModel> _selectedInputFiles = new ObservableCollection<FileViewModel>();
+        private readonly ObservableCollection<NotificationListItemViewModel> _notificationListItems = new ObservableCollection<NotificationListItemViewModel>();
+
+        public ObservableCollection<FileViewModel> SelectedInputFiles => _selectedInputFiles;
+
+        public ObservableCollection<NotificationListItemViewModel> NotificationListItems => _notificationListItems;
+
+        public ICommand StartExport { get; }
+        public ICommand CloseProgramm { get; }
+        public ICommand SelectOutputFilePath { get; }
+        public ICommand AddNewSourceFile { get; }
+        public ICommand RemoveSourceFile { get; }
+        public ICommand OpenOutputFile { get; }
+
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
