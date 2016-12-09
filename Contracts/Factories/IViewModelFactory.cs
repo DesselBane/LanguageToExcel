1092ca8fc4a0176faf5de4c4d6dd08a6d0c34915
D4rthB4n3 <dessel.bane@gmx.de>
2016-12-09 15:13:51 +0100

    Added ViewModelFactory and ViewModelBase interfaces

---
+++ Contracts/Factories/IViewModelFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts.Presentation;
+
+namespace Contracts.Factories
+{
+    public interface IViewModelFactory
+    {
+        TEntity GetViewModel <TEntity>() where TEntity : IViewModelBase;
+    }
+}
