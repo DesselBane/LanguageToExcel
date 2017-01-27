e9891a5ca1d861435c2d7348dc125ed2a8d888c0
D4rthB4n3 <dessel.bane@gmx.de>
2017-01-27 14:14:47 +0100

    implemented PropertiesFileService documented IPropertiesFileService

--- ExcelExport.Contracts/Services/IPropertiesFileService.cs
+++ ExcelExport.Contracts/Services/IPropertiesFileService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using ExcelExport.Contracts.Model;
 
@@ -6,8 +7,25 @@ namespace ExcelExport.Contracts.Services
 {
     public interface IPropertiesFileService
     {
+        /// <summary>
+        /// Adds a File to be tracked as a Properties File
+        /// </summary>
+        /// <param name="fileInfo">File Path to the properties File</param>
+        /// <returns>Returns an instance of IPropertiesFile</returns>
+        /// <exception cref="ArgumentException">Thrown if there is a Properties file with the same FilePath</exception>
         IPropertiesFile AddPropertiesFile(FileInfo fileInfo);
+
+        /// <summary>
+        /// Removes a File from the tracked Property Files Collection
+        /// </summary>
+        /// <param name="file">The Propertyfile to remove</param>
+        /// <exception cref="ArgumentException">Thrown if the file instance doesnt exist</exception>
         void RemovePropertiesFile(IPropertiesFile file);
+
+        /// <summary>
+        /// Returns all tracked Files
+        /// </summary>
+        /// <returns>Returns all tracked Files</returns>
         IEnumerable<IPropertiesFile> RegisteredFiles();
     }
 }
--- ExcelExport/Model/PropertiesFileService.cs
+++ ExcelExport/Model/PropertiesFileService.cs
@@ -1,29 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using ExcelExport.Contracts;
+using ExcelExport.Contracts.Events;
 using ExcelExport.Contracts.Model;
 using ExcelExport.Contracts.Services;
+using Prism.Events;
 
 namespace ExcelExport.Model
 {
     public class PropertiesFileService : IPropertiesFileService
     {
+        private List<IPropertiesFile> _propertiesFiles = new List<IPropertiesFile>();
+        private EventAggregator _eventAggregator;
+
+        public PropertiesFileService(EventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
+
         #region Implementation of IExportPropertiesFileService
 
         public IPropertiesFile AddPropertiesFile(FileInfo fileInfo)
         {
-            throw new NotImplementedException();
+            if(_propertiesFiles.Any(x => x.FilePath.FullName == fileInfo.FullName))
+                throw new ArgumentException("This file path is already added");
+
+            
+
+            var propFile = new PropertiesFile {FilePath = fileInfo};
+            _propertiesFiles.Add(propFile);
+
+            _eventAggregator.GetEvent<PubSubEvent<AddedPropertiesFileEvent>>().Publish(new AddedPropertiesFileEvent(propFile));
+
+            return propFile;
         }
 
         public void RemovePropertiesFile(IPropertiesFile file)
         {
-            throw new NotImplementedException();
+            if(!_propertiesFiles.Contains(file))
+                throw new ArgumentException("No such file is tracked");
+
+            _propertiesFiles.Remove(file);
+
+            _eventAggregator.GetEvent<PubSubEvent<RemovedPropertiesFileEvent>>().Publish(new RemovedPropertiesFileEvent(file));
         }
 
         public IEnumerable<IPropertiesFile> RegisteredFiles()
         {
-            throw new NotImplementedException();
+            return _propertiesFiles.ToArray();
         }
 
         #endregion
