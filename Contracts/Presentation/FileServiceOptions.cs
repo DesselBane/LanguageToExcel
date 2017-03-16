f12a7c3acb4d66b2f9e17b68bd11e52108faf141
D4rthB4n3 <dessel.bane@gmx.de>
2017-03-16 09:19:11 +0100

    Worked on Buttons and FileService

---
+++ Contracts/Presentation/FileServiceOptions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Presentation
+{
+    public class FileServiceOptions : IFileServiceOptions
+    {
+        private bool _dereferenceLinks;
+        private string _defaultExtensions;
+        private string _filter;
+        private string _initialDirectory;
+        private string _title;
+
+        #region Implementation of IFileServiceOptions
+
+        public bool DereferenceLinks
+        {
+            get { return _dereferenceLinks; }
+            set { _dereferenceLinks = value; }
+        }
+
+        public string DefaultExtensions
+        {
+            get { return _defaultExtensions; }
+            set { _defaultExtensions = value; }
+        }
+
+        public string Filter
+        {
+            get { return _filter; }
+            set { _filter = value; }
+        }
+
+        public string InitialDirectory
+        {
+            get { return _initialDirectory; }
+            set { _initialDirectory = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        #endregion
+    }
+}
--- ExcelExport/ViewModels/ControlsViewModel.cs
+++ ExcelExport/ViewModels/ControlsViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using Contracts.Presentation;
 using ExcelExport.Contracts.Services;
@@ -14,57 +12,132 @@ namespace ExcelExport.ViewModels
 {
     public class ControlsViewModel : ViewModelBase
     {
-        private FileInfo _outputPath;
-        private DelegateCommand _selectOutputPathCommand;
-        private IFileService _fileService;
-        private DelegateCommand _runCommand;
+        #region Vars
+
         private IExcelExportService _excelExportService;
+        private IFileService _fileService;
         private DelegateCommand _openCommand;
+        private FileInfo _outputPath;
+        private DelegateCommand _runCommand;
+        private DelegateCommand _selectOutputPathCommand;
+        private bool _isWorking;
+
+        #endregion
+
+        #region Properties
 
         public string OutputPath => _outputPath?.FullName;
-        public ICommand SelectOutputPath => _selectOutputPathCommand ?? (_selectOutputPathCommand = new DelegateCommand(OnSelectOutputPath,CanSelectOutputPath));
+        public ICommand SelectOutputPath => _selectOutputPathCommand ?? (_selectOutputPathCommand = new DelegateCommand(OnSelectOutputPath, CanSelectOutputPath));
         public ICommand Run => _runCommand ?? (_runCommand = new DelegateCommand(OnRun, CanRun));
         public ICommand Open => _openCommand ?? (_openCommand = new DelegateCommand(OnOpen, CanOpen));
+
+        public bool IsWorking
+        {
+            get { return _isWorking; }
+            set
+            {
+                _isWorking = value;
+                FirePropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Constructors
 
         public ControlsViewModel(IFileService fileService, IExcelExportService excelExportService)
         {
             _fileService = fileService;
             _excelExportService = excelExportService;
+
+            PropertyChanged += OnPropertyChanged;
         }
+
+        #endregion
+
+        #region Events
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(IsWorking):
+                    _selectOutputPathCommand.RaiseCanExecuteChanged();
+                    _runCommand.RaiseCanExecuteChanged();
+                    _openCommand.RaiseCanExecuteChanged();
+                    break;
+
+                case nameof(OutputPath):
+                    _openCommand.RaiseCanExecuteChanged();
+                    _runCommand.RaiseCanExecuteChanged();
+                    break;
+            }
+        }
+
+        #endregion Events
 
         #region Command Handler
 
         private void OnSelectOutputPath()
         {
-            _outputPath = _fileService.SaveFile();
+            IsWorking = true;
+            FileInfo newOutput = _fileService.SaveFile(new FileServiceOptions
+            {
+                DefaultExtensions = ".xlsx",
+                DereferenceLinks = true,
+                Filter = "Excel Workbook|*.xlsx"
+            });
+
+            if (newOutput == null)
+            {
+                IsWorking = true;
+                return;
+            }
+            _outputPath = newOutput;
+
+            if(_outputPath.Extension != ".xlsx")
+                _outputPath = new FileInfo(_outputPath.FullName + ".xlsx");
+
             FirePropertyChanged(nameof(OutputPath));
+            IsWorking = false;
         }
+
         private bool CanSelectOutputPath()
         {
-            return true;
+            return !IsWorking;
         }
 
         private async void OnRun()
         {
+            IsWorking = true;
             await _excelExportService.ExportToExcelAsync(_outputPath);
+            _outputPath.Refresh();
+            IsWorking = false;
         }
 
         private bool CanRun()
         {
-            return true;
+            return !IsWorking && !string.IsNullOrWhiteSpace(OutputPath);
         }
 
         private void OnOpen()
         {
-            System.Diagnostics.Process.Start(OutputPath);
+            try
+            {
+                Process.Start(OutputPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                //throw new NotImplementedException();
+            }
         }
 
         private bool CanOpen()
         {
-            return true;
+            return !IsWorking && !string.IsNullOrWhiteSpace(OutputPath) && OutputPath.EndsWith(".xlsx") && _outputPath != null && _outputPath.Exists;
         }
 
         #endregion Command Handler
-
     }
 }
